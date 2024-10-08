f5e51fd3851511435bd1ec63f11bea5339cda20b
adoumasseo <adoumasseo@gmail.com>
2024-10-08 09:14:15 +0100

    Eslint standard check

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
@@ -6,7 +6,5 @@ export default function handleProfileSignup() {
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline'); 
-    });
+    .catch(() => console.log('Signup system offline'));
 }
