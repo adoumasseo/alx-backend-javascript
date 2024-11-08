c2390127f73a0648ca8e4482de961fd89bc0c5f0
adoumasseo <adoumasseo@gmail.com>
2024-11-08 16:02:08 +0100

    First Task

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const firstObject = {id: 1, firstName: 'Guillaume', location: 'San Francisco'}
+  const secondObject = {id: 2, firstName: 'James', location: 'Columbia'}
+  const lastObject = {id: 5, firstName: 'Serena', location: 'San Francisco'}
+
+  return [firstObject, secondObject, lastObject]
+}
