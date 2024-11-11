8dd968a71042ca4e9453293c1cacaca562fea8af
adoumasseo <adoumasseo@gmail.com>
2024-11-11 03:25:00 +0100

    Fix: Remove unary operator and use every() instead of for basic lopp

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
 export default function hasValuesFromArray(theSet, theArray) {
-  for (let i = 0; i < theArray.length; i++) {
-    if (!theSet.has(theArray[i])) {
-      return false;
-    }
-  }
-  return true;
+  theArray.every((value) => theSet.has(value));
 }
