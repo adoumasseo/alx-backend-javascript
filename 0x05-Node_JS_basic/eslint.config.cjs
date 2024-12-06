e3eaff3f44b89a0a9b166f7fc228a1efd138a6dd
adoumasseo <adoumasseo@gmail.com>
2024-12-06 16:45:25 +0100

    Init commit

---
+++ 0x05-Node_JS_basic/eslint.config.cjs
@@ -0,0 +1,18 @@
+// eslint.config.cjs
+module.exports = [
+  {
+    ignores: ["node_modules/"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "eqeqeq": "error",
+    },
+  },
+];
+
