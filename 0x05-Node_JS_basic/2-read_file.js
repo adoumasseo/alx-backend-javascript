84ee63db1d133683812a9f0f4c40c29e8b79c551
adoumasseo <adoumasseo@gmail.com>
2024-12-08 17:41:08 +0100

    File Reading with nodeJs and Data manipulation

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  const studentStats = {};
+  let studentStatsKeys;
+  let studentStatsValues;
+  let course;
+  try {
+    const students = fs.readFileSync(path, 'utf8');
+    if (students) {
+      const arrayStudents = students.split('\n').slice(1);
+      arrayStudents.forEach((studentLine) => {
+        const studentInfo = studentLine.split(',');
+        studentStatsKeys = Object.keys(studentStats);
+        course = studentInfo[studentInfo.length - 1];
+        if (!studentStatsKeys.includes(course)) {
+          studentStats[course] = [studentInfo[0]];
+        } else {
+          studentStatsValues = studentStats[course];
+          studentStats[course] = [...studentStatsValues, studentInfo[0]];
+        }
+      });
+      console.log(`Number of students: ${arrayStudents.length}`);
+      studentStatsKeys = Object.keys(studentStats);
+      studentStatsKeys.forEach((element) => {
+        console.log(`Number of students in ${element}: ${studentStats[element].join(', ')}`);
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
