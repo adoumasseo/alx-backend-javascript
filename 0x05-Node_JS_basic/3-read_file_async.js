f44a456a47dd38ecd9b16b203e92cd3e159d7ea6
adoumasseo <adoumasseo@gmail.com>
2024-12-08 19:23:05 +0100

    Read the database but asynchronously

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, students) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const studentsString = students.toString();
+        const studentStats = {};
+        const arrayStudents = studentsString.split('\n').slice(1);
+        arrayStudents.forEach((studentLine) => {
+          const studentInfo = studentLine.split(',');
+          const course = studentInfo[studentInfo.length - 1];
+          if (!Object.prototype.hasOwnProperty.call(studentStats, course)) {
+            studentStats[course] = [studentInfo[0]];
+          } else {
+            studentStats[course] = [...studentStats[course], studentInfo[0]];
+          }
+        });
+        console.log(`Number of students: ${arrayStudents.length}`);
+        Object.keys(studentStats).forEach((key) => {
+          console.log(
+            `Number of students in ${key}: ${studentStats[key].length}. List: ${studentStats[key].join(', ')}`,
+          );
+        });
+        resolve(true);
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
