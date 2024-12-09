39a8e287e4ddcf0eaf9152b677583e181f253c07
adoumasseo <adoumasseo@gmail.com>
2024-12-09 09:40:17 +0100

    Fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,18 +1,20 @@
-const { createServer } = require('node:http');
+const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
-const app = createServer((req, res) => {
-  res.statusCode = 200;
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', 'Hello Holberton School!'.length);
-  res.write('Hello Holberton School!');
-  res.end();
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-app.listen(port, hostname, () => {
-  process.stdout.write(`Server listening at -> http://${hostname}:${port}\n`);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
