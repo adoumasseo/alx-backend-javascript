446cfe76befeb450051611a065ac1482a8c7c24a
adoumasseo <adoumasseo@gmail.com>
2024-12-09 16:26:50 +0100

    Don't know why it's not working

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,8 +1,5 @@
 import readDatabase from '../utils';
 
-/**
- * The list of supported majors.
- */
 const VALID_MAJORS = ['CS', 'SWE'];
 
 class StudentsController {
@@ -12,8 +9,6 @@ class StudentsController {
     readDatabase(dataPath)
       .then((studentGroups) => {
         const responseParts = ['This is the list of our students'];
-        // A comparison function for ordering a list of strings in ascending
-        // order by alphabetic order and case insensitive
         const cmpFxn = (a, b) => {
           if (a[0].toLowerCase() < b[0].toLowerCase()) {
             return -1;
