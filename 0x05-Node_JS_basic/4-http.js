f3b9044b733e729c8c5a27c8ee8b096592cd1283
adoumasseo <adoumasseo@gmail.com>
2024-12-09 09:34:02 +0100

    Try loggin 'Server listening at -> url' on the stdout

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,18 @@
-const http = require('node:http');
+const { createServer } = require('node:http');
 
-const app = http.createServer((req, res) => {
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', 'Hello Holberton School!'.length);
   res.write('Hello Holberton School!');
   res.end();
 });
-app.listen(1245);
+
+app.listen(port, hostname, () => {
+  process.stdout.write(`Server listening at -> http://${hostname}:${port}\n`);
+});
+
 module.exports = app;
